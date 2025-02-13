701dffe68df9aa7396518b97dac27ee2c9bd36be
Stephan Grein <stephan.grein@uni-bonn.de>
2025-02-13 15:36:00 +0100

    Adding PyPI package upload.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+def load_requirements(filename):
+    """Load dependencies from requirements.txt"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return [
+            line.strip()
+            for line in f
+            if line.strip() and not line.startswith("#")
+        ]
+
+
+setup(
+    name="calorimetry_tools",
+    version="0.1.0",
+    install_requires=load_requirements("requirements.txt"),
+)
